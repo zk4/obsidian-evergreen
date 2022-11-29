eea359f8280f230b0e2cbf930d9df2533af7b64b
zk <liuzq7@gmail.com>
2022-11-29 13:59:44 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,58 @@
+# Obsidian "EverGreen" Plugin
+
+![Custom badge](https://img.shields.io/endpoint?url=https%3A%2F%2Fscambier.xyz%2Fobsidian-endpoints%2Fno-dupe-leaves.json)
+
+[EverGreen](https://notes.andymatuschak.org/) workflow with stack tabs in Obsidian 
+
+
+
+## Support Obsidian version
+
+\>= 1.0.0
+
+## How to use
+
+1. install this plugin 
+2. enable `Stack tabs`
+
+## Feature
+
+1 Open internal link in stacked tab;
+
+
+2 Reuse already opened tab if possible;
+
+   
+
+   
+
+3 Keep only leaf to parent full tab history from link hierarchy, otherwise tab would be deleted.
+
+
+
+Ex: Suppose you have these files in link:
+
+A
+  - B
+    - C
+    - D
+      - H
+      - C
+    - E
+  - F
+    - G
+
+When I open A->B->D->C  then click D in B ， C would be delete,
+
+
+
+
+
+ 
+
+plugin inspired by: [No Dupe Leaves](https://github.com/scambier/obsidian-no-dupe-leaves)
+
+ 
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,68 @@
+import {OpenViewState, Plugin, Workspace} from 'obsidian'
+import {around} from 'monkey-around'
+
+
+let uninstallPatchOpen: () => void
+
+// safely handles circular references
+export default class NoDupeLeavesPlugin extends Plugin {
+  async onload(): Promise<void> {
+    uninstallPatchOpen = around(Workspace.prototype, {
+
+
+      // Monkey-patch the OpenLinkText function
+      openLinkText(oldOpenLinkText) {
+        return function (
+          linktext: string,
+          sourcePath: string,
+          newLeaf?: boolean,
+          openViewState?: OpenViewState,
+        ) {
+          // Make sure that the path ends with '.md'
+          const name = linktext + (linktext.endsWith('.md') ? '' : '.md')
+          let result
+          let found = false
+          let dirtyIndex = 0
+          const tabs = app.workspace.getLeavesOfType('markdown')
+          for (let i = 0; i < tabs.length; i++) {
+            let leaf = tabs[i]
+            const viewState = leaf.getViewState()
+            if (viewState.type === 'markdown') {
+              // Found a corresponding pane
+              if (viewState.state?.file?.endsWith(name)) {
+                found = true
+                app.workspace.setActiveLeaf(leaf)
+              }
+              // found current dirt index
+              if (viewState.state?.file?.endsWith(sourcePath)) {
+                dirtyIndex = i
+              }
+            }
+          }
+
+          // close all remaining tabs from dirtyIndex if hierarchy line changes
+          if (tabs.length > dirtyIndex && !found) {
+            for (let i = dirtyIndex + 1; i < tabs.length; i++) {
+              tabs[i].detach()
+            }
+          }
+
+          // If no pane matches the path, call the original function
+          result =
+            oldOpenLinkText &&
+            oldOpenLinkText.apply(this, [
+              linktext,
+              sourcePath,
+              !found,
+              openViewState,
+            ])
+          return result
+        }
+      },
+    })
+  }
+
+  onunload(): void {
+    uninstallPatchOpen()
+  }
+}
